8bdad6f90b2bb95dca11a6a1a797e82335e0556a
André Escobar <83234248+Impxdent@users.noreply.github.com>
2025-04-20 14:51:44 -0600

    Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package karnaugh;
+
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new InterfazGrafica().setVisible(true);
+        });
+    }
+}
